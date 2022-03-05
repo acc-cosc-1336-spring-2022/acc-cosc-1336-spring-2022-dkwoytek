77bfdb928644472db4929c923996ebd10fd3eb33
dkwoytek <david.woytek@g.austincc.edu>
2022-03-05 14:02:11 -0600

    homework 4

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,44 @@
-#
+def get_hour(epoch_seconds):
+    return epoch_seconds // 3600
+
+def get_minutes(epoch_seconds):
+    return (epoch_seconds // 60) % 60
+
+def get_seconds(epoch_seconds):
+    return epoch_seconds % 60
+
+def time_from_utc(utc_offset, utc_zero):
+    return utc_offset + utc_zero
+
+def get_time(hour, minutes, seconds, time_type, meridiem='AM'):
+    if minutes < 0 or minutes > 59:
+        return 'Invalid minutes(range 0-59)'
+
+    if seconds < 0 or seconds > 59:
+        return 'Invalid seconds(range 0-59)'
+
+    if time_type == 24:
+        if hour < 0 or hour > 23:
+            return 'Invalid hours(range 0-23)'
+
+    elif time_type == 12:
+        if hour < 0 or hour > 12:
+            return 'Invalid hours(range 1-12)'
+    else:
+        return 'Invalid time_type(12 or 24 only)'
+
+    if hour < 10:
+        hour = "0" + str(hour)
+
+    if minutes < 10:
+        minutes  = "0" + str(minutes)
+
+    if seconds < 10:
+        seconds = "0" + str(seconds)
+
+    time = str(hour) + ":" + str(minutes) + ":" + str(seconds)
+
+    if time_type == 12:
+        time = time + " " + meridiem
+
+    return  time
--- tests/homework/e_functions/tests_functions.py
+++ tests/homework/e_functions/tests_functions.py
@@ -1,1 +1,51 @@
-#
+import unittest
+
+
+'''
+The file at /src/homework/e_in_proc_out/output has 
+the function get_hour.
+'''
+from src.homework.e_functions.value_return import get_hour
+from src.homework.e_functions.value_return import get_minutes
+from src.homework.e_functions.value_return import get_seconds
+from src.homework.e_functions.value_return import time_from_utc
+
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_hour(self):
+        #test that the function get_factorial with parameter value 3800 returns the value 1
+        self.assertEqual(1, get_hour(3800))
+
+    def test_get_hour(self):
+        #test that the function get_factorial with parameter value 3600 returns the value 1
+        self.assertEqual(1, get_hour(3600))
+
+    def test_get_minutes(self):
+        #test that the function get_factorial with parameter value 3800 returns the value 3
+        self.assertEqual(3, get_minutes(3800))
+
+    def test_get_minutes(self):
+        #test that the function get_factorial with parameter value 3600 returns the value 0
+        self.assertEqual(0, get_minutes(3600))
+
+    def test_get_seconds(self):
+        #test that the function get_factorial with parameter value 3800 returns the value 20
+        self.assertEqual(20, get_seconds(3800))
+
+    def test_get_seconds(self):
+        #test that the function get_factorial with parameter value 3600 returns the value 0
+        self.assertEqual(0, get_seconds(3600))
+
+    def test_utc_time_to_eastern_standard_time(self):
+        self.assertEqual(15, time_from_utc(-5, 20))
+    
+    def test_utc_time_to_central_standard_time(self):
+        self.assertEqual(14, time_from_utc(-6, 20))
+
+    def test_utc_time_to_mountain_standard_time(self):
+        self.assertEqual(13, time_from_utc(-7, 20)) 
+
+    def test_utc_time_to_pacific_standard_time(self):
+        self.assertEqual(12, time_from_utc(-8, 20))  
+          
