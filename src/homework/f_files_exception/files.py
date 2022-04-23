73ffc559be05531c9636d727776672413968a133
dkwoytek <david.woytek@g.austincc.edu>
2022-04-23 18:21:12 -0500

    HW 8

--- src/homework/f_files_exception/files.py
+++ src/homework/f_files_exception/files.py
@@ -1,1 +1,32 @@
-#
+def add_inventory(inventory, widget_name, widget_quantity):
+    if widget_name not in inventory:            #add the widget to the inventory
+        inventory[widget_name] = widget_quantity
+    else:                                       #update the inventory of widgets 
+        inventory[widget_name] += widget_quantity
+    
+def remove_inventory_widget(inventory, widget_name):
+    if widget_name in inventory:
+        del inventory[widget_name]
+        print('Record deleted')
+    else:
+        print('Item not found.')
+
+def write_to_file(inventory):
+    inventory_file = open('inventory.txt', 'w')
+    for key, value in inventory.items():
+        inventory_file.write(key + '\n')
+        inventory_file.write(str(value) + '\n')
+    inventory_file.close()
+
+def read_from_file():
+    try:
+        inventory_file = open('inventory.txt', 'r') 
+        inventory_item = inventory_file.readline()
+        while inventory_item != '': # reads from file and displays items
+            inventory_quantity = inventory_file.readline()
+            print('Inventory item:', inventory_item.rstrip('\n'), end='  ')
+            print('Inventory quantity:', inventory_quantity.rstrip('\n'))
+            inventory_item = inventory_file.readline()
+        inventory_file.close
+    except IOError:
+        print('This inventory file does not exist.')
--- src/homework/f_files_exception/main.py
+++ src/homework/f_files_exception/main.py
@@ -1,1 +1,30 @@
-#
+import files
+
+inventory = {}
+menu_selex = "y"
+
+while menu_selex == "y":   #This is so that HW 7 Menu is generated
+    print('\nHomework 7 Menu\n1-Add or Update Item\n2-Display Items\n3-Exit')
+    selex = int(input('Please select menu item 1, 2 or 3: ')) 
+
+    if selex == 1:   #This is the Add or Update Item part of the assignment
+        print('Please add or update your inventory item.')
+        widget_name = input('Enter name of item: ')
+        try:
+          widget_quantity = int(input('Enter quantity of item added to inventory (as a positive number) or sold from inventory (as negative number): '))
+          files.add_inventory(inventory, widget_name, widget_quantity)
+        except ValueError:
+             print('Data entry error. Quantity entered must be entered as a numeric integer data amount.')
+        
+    elif selex == 2:   #This is the Display Items part of the assignment
+         files.write_to_file(inventory)
+         files.read_from_file()
+
+    elif selex == 3:   #This is the exit from the menu part of the assignment
+         print('You have chosen to exit.')
+    
+    else:
+         print("Invalid entry")
+
+    menu_selex = input('Do you want to continue with another menu selection (Enter "y" for yes, n to exit from Homework 7 Menu): ')
+
